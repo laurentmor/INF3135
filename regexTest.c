bb8d02902f976e9a6146aa07f1136f2eb4430b0c
Laurent Morissette <Laurent.Morissette@gmail.com>
2013-10-28 11:27:52 -0400

    Update regexTest.c

--- regexTest.c
+++ regexTest.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
 {
 	int erreur;
 	const char * chaine1="-12";
-	const char * expr="(^-{1}\\d+$){1}|(\\d+-{1}$){1}|(^\\d+-{1}\\d+$){1}";
+	const char * expr="(^-{1}[0-9]+$){1}|(^[0-9]+-$)|(^[0-9]+-[0-9]+$)";
     regex_t expressionPtr;
 	erreur=regcomp(&expressionPtr,expr,REG_NOSUB | REG_EXTENDED);
 	if(erreur==REGEXOK){
