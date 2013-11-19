d0d76aaf7c2ed29c38ac5fe58ffbe6ce2da4aa8e
laurentmor <laurent.morissette@gmail.com>
2013-11-19 17:06:57 -0500

    module de regex

--- TP2/mod_regex.c
+++ TP2/mod_regex.c
@@ -17,7 +17,7 @@ int valider(char *expression, regex_t regPtr ){
 int chercherDansChaine(char *expression, char *chaine){
 	regex_t regPtr;
 	if(valider(expression,regPtr)==REGEX_VALIDE){
-		int trouve=regexec(&regPtr,expression,0,NULL,0);
+		int trouve=regexec(&regPtr,chaine,0,NULL,0);
 		regfree(&regPtr);
 		if (trouve==0)return 1;
 		else return 0;
