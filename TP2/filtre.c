4b28a288a4008a869d95dbb8cf393ca4983f1885
laurentmor <laurent.morissette@gmail.com>
2013-11-23 16:11:20 -0500

    mod_domaine

--- TP2/filtre.c
+++ TP2/filtre.c
@@ -1,4 +1,9 @@
 #include "mod_regex.h"
+#define TOUTE_SYNTAXE_VALIDE "(^-{1}[0-9]+$){1}|(^[0-9]+-$)|(^[0-9]+-[0-9]+$)"
+#define _NUM "(^-{1}[0-9]+$){1}"
+#define NUM_ "(^[0-9]+-$)"
+#define NUM1_NUM2 "(^[0-9]+-[0-9]+$)"  
+
 int main()
 {
 	
@@ -6,12 +11,8 @@ int main()
 pour fin de tests
  definition des type de domaines
 */
-#define TOUTE_SYNTAXE_VALIDE "(^-{1}[0-9]+$){1}|(^[0-9]+-$)|(^[0-9]+-[0-9]+$)"
-#define _NUM "(^-{1}[0-9]+$){1}"
-#define NUM_ "(^[0-9]+-$)"
-#define NUM1_NUM2 "(^[0-9]+-[0-9]+$)"  
- fprintf(stdout, "%d\n",chercherDansChaine(_NUM,"-12") );
- fprintf(stdout, "%d\n",chercherDansChaine(NUM_,"12-") );
- fprintf(stdout, "%d\n",chercherDansChaine(NUM1_NUM2,"1-12") );    
+ printf("%d\n",chercherDansChaine(_NUM,"-12") );
+ printf("%d\n",chercherDansChaine(NUM_,"12-") );
+ printf("%d\n",chercherDansChaine(NUM1_NUM2,"1-12") );    
 	return 0;
 }
