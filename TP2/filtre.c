1743c07839deb0d700048a64599f2c758da5dfdd
laurentmor <laurent.morissette@gmail.com>
2013-11-23 15:23:08 -0500

    mod_domaine

--- TP2/filtre.c
+++ TP2/filtre.c
@@ -1,7 +1,17 @@
 #include "mod_regex.h"
-int main(int argc, char const *argv[])
+int main()
 {
-	regex_t reg;
-	valider(" ",reg);
+	
+/**
+pour fin de tests
+ definition des type de domaines
+*/
+#define TOUTE_SYNTAXE_VALIDE "(^-{1}[0-9]+$){1}|(^[0-9]+-$)|(^[0-9]+-[0-9]+$)"
+#define _NUM "(^-{1}[0-9]+$){1}"
+#define NUM_ "(^[0-9]+-$)"
+#define NUM1_NUM2 "(^[0-9]+-[0-9]+$)"  
+ fprintf(stdout, "%d\n",chercherDansChaine(_NUM,"-12") );
+ fprintf(stdout, "%d\n",chercherDansChaine(NUM_,"12-") );
+ fprintf(stdout, "%d\n",chercherDansChaine(NUM1_NUM2,"1-12") );    
 	return 0;
 }
