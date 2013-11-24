c36d5d53be747c9e79797c1b0c2c000c7fe51565
laurentmor <laurent.morissette@gmail.com>
2013-11-23 19:43:57 -0500

    erreur

--- TP2/filtre.c
+++ TP2/filtre.c
@@ -1,4 +1,4 @@
-#include "mod_regex.h"
+#include "mod_erreur.h"
 #define TOUTE_SYNTAXE_VALIDE "(^-{1}[0-9]+$){1}|(^[0-9]+-$)|(^[0-9]+-[0-9]+$)"
 #define _NUM "(^-{1}[0-9]+$){1}"
 #define NUM_ "(^[0-9]+-$)"
@@ -7,12 +7,7 @@
 int main()
 {
 	
-/**
-pour fin de tests
- definition des type de domaines
-*/
- printf("%d\n",chercherDansChaine(_NUM,"-12") );
- printf("%d\n",chercherDansChaine(NUM_,"12-") );
- printf("%d\n",chercherDansChaine(NUM1_NUM2,"1-12") );    
+signaler(OPTION_ERREUR);
+
 	return 0;
 }
