799f7b79f12898e1ca9da7904098eec1d9e16d2a
laurentmor <laurent.morissette@gmail.com>
2013-12-09 21:38:17 -0500

    mod_fichier

--- TP2/mod_fichier.h
+++ TP2/mod_fichier.h
@@ -3,5 +3,5 @@
 #include <stdlib.h>
 #include <stdio.h> 
 int nombreDeLignes(FILE* fp);
-char ** charger(FILE *fp, char *vecteur, int nbLignes, int nbColonnes);
+char ** charger(FILE *fp, char **vecteur, int nbLignes, int nbColonnes);
 #endif
